fe764e10b0c03397f74bc0992967473293c55b07
Gustavo Valentim <guuss.valentim@outlook.com>
2023-04-10 23:15:12 -0300

    v4
    codigo do corno master

--- ap1/Cliente.py
+++ ap1/Cliente.py
@@ -1,4 +1,5 @@
 from socket import *
+import os
 
 
 class PTATClient():
@@ -10,73 +11,104 @@ class PTATClient():
         self.clientSocket = socket(AF_INET, SOCK_STREAM)
         self.clientSocket.connect((self.serverName, self.serverPort))
 
-    def nova_requisicao(req):
+    def nova_requisicao(self, req):
         lista_req = req.split()
         op = lista_req[0]
         
+        match op:
+            case 'read':
+                # read path/filename 
+                op = 0
+                str_pathRemoto = lista_req[1]
+            
+                lista_path = str_pathRemoto.split("/")
+            
+                filename = lista_path.pop()
+                path = "/".join(lista_path)
+                body = ""
+                length = 0
+
+            case 'write':
+                op = 1
+            
+                str_pathLocal = lista_req[1]
+
+                
+                with open(str_pathLocal, 'r') as file:
+                   body = file.read()
+                
+                length = os.path.getsize(str_pathLocal)
+
+                str_pathRemoto = lista_req[2]
+                lista_pathRemoto = str_pathRemoto.split("/")
+            
+                filename = lista_pathRemoto.pop()
+                path = "/".join(lista_path)
+
+
+            case 'del':
+                # del caminho_arquivo/nome_arquivo_remoto
+                op = 2
+                str_pathRemoto = lista_req[1]
+
+                lista_pathRemoto = str_pathRemoto.split("/")
+                
+                filename = lista_pathRemoto.pop()
+                path = "/".join(lista_path)
+
+                length = 0
+                body = ""
+
+                # fazer length
+
+            case 'list':
+                # list caminho_remoto
+                op = 3
+                pathRemoto = lista_req[1]
+
+                length = 0
+                filename = ""
+                body = ""
+
+            case _:
+                op = 4
+                length = 0
+                body = ""
+                filename = ""
+                path = ""
+
+        self.op = op
+        self.length = length
+        self.filename = filename
+        self.path = path
+        self.body = body
+
+
+    def enviar_requisicao(self):
+        self.clientSocket.send(str(self.op).encode())
+        self.clientSocket.send(str(self.length).encode())
+        self.clientSocket.send(self.filename.encode())
+        self.clientSocket.send(self.path.encode())
+        self.clientSocket.send(self.body.encode())
+
+    def recebe_respostas(self):
+        self.op = self.clientSocket.recv(1).decode()
+        self.length = self.clientSocket.recv(6).decode()
+        self.filename = self.clientSocket.recv(64).decode()
+        self.path = self.clientSocket.recv(128).decode()
+        self.code = self.clientSocket.recv(1).decode()
+        self.message = self.clientSocket.recv(128).decode()
+        self.body = self.clientSocket.recv(self.length).decode()
+
+def main():
+        cliente = PTATClient()
         
-        if(op == 'read'):
-            # read path/filename 
-            op = 0
-            str_pathRemoto = lista_req[1]
-            
-            lista_path = str_pathRemoto.split("/")
-            
-            filename = lista_path.pop()
-            
-            path = "/".join(lista_path)
-
-
-            
-            body = ""
-
-
-
-        elif(op == 'write'):
-            # write path/filename_local path/filename_server
-            op = 1
-            
-            str_pathLocal = lista_req[1]
-            lista_pathLocal = str_pathLocal.split("/")
-            
-            filenameLocal = lista_pathLocal.pop()
-            pathLocal = "/".join(lista_path)
-
-            str_pathRemoto = lista_req[2]
-            lista_pathRemoto = str_pathRemoto.split("/")
-            
-            filenameRemoto = lista_pathRemoto.pop()
-            pathRemoto = "/".join(lista_path)
-
-            # fazer length
-
-
-
-        elif(op == 'del'):
-            # del caminho_arquivo/nome_arquivo_remoto
-            op = 2
-            str_pathRemoto = lista_req[1]
-
-            lista_pathRemoto = str_pathRemoto.split("/")
-            filenameRemoto = lista_pathRemoto.pop()
-            pathRemoto = "/".join(lista_path)
-
-            # fazer length
-
-
-        elif(op == 'list'):
-            # list caminho_remoto
-            op = 3
-            pathRemoto = lista_req[1]
-
-
-        else:
-            op = -1
-            #
-
-
-
-
-
-    def main():
         while True:
+
+            req = input("Digite uma requisição: ")
+
+            cliente.nova_requisicao(req)
+            cliente.enviar_requisicao()
+            cliente.recebe_respostas()
+
+        cliente.clientSocket.close()
--- ap1/Server.py
+++ ap1/Server.py
@@ -1,4 +1,6 @@
 from socket import *
+import os
+import datetime
 
 
 class PTATServer():
@@ -9,3 +11,126 @@ class PTATServer():
         self.serverSocket = socket(AF_INET, SOCK_STREAM)
         self.serverSocket.bind((self.host, self.serverPort))
         self.serverSocket.listen(1)
+
+
+    def realiza_requisicao(self):
+        match(self.op):
+            case 0:
+                # read
+                caminho_arq = str(self.path + self.filename)
+                try:
+                    with open(caminho_arq, 'r') as file:
+                        
+                        self.length = os.path.getsize(caminho_arq)
+                        self.body = file.read()
+
+                        self.code = 0
+                        self.message = "Arquivo lido com sucesso"
+                
+                except FileNotFoundError:
+                    self.length = 1  
+                    self.code = 2      
+                    self.body = ""
+                    self.message = "Nome de arquivo não existente no servidor"
+                    raise
+
+
+            case 1:
+                # write
+                caminho_arq = str(self.path + self.filename)
+
+                try:
+                    with open(caminho_arq, 'w') as file:
+                        file.write(self.body)
+                except FileNotFoundError:
+                    self.length = 1
+                    self.code = 1
+                    self.message = "Caminho não existente no servidor"
+                    self.body = ""
+                    raise
+
+            case 2:
+                # delete
+                caminho_arq = str(self.path + self.filename)
+                
+                try:
+                    os.remove(caminho_arq)
+
+                except FileNotFoundError:
+                    self.length = 1
+                    self.code = 1
+                    self.message = "Caminho não existente no servidor"
+                    self.body = ""
+                    raise
+
+            case 3:
+                # list
+                diretorio = self.path
+                try:
+                    arquivos_diretorio = os.listdir(diretorio)
+                    
+                    for arquivo in arquivos_diretorio:
+                        body = body + "\n" + arquivo
+                    
+                except FileNotFoundError:
+                    self.length = 1
+                    self.code = 1
+                    self.message = "Caminho não existente no servidor"
+                    self.body = ""
+                    raise
+                    
+
+            case _:
+                self.length = 1
+                self.code = 4
+                self.message = "Operação inválida"
+                self.body = ""
+
+        horario = datetime.datetime.now().strftime("%H:%M:%S")
+
+        yield horario, self.op, self.path, self.code
+
+    def enviar_resposta(self):
+        self.connectionSocket.send(str(self.op).encode())
+        self.connectionSocket.send(str(self.length).encode())
+        self.connectionSocket.send(str(self.filename).encode())
+        self.connectionSocket.send(str(self.path).encode())
+        self.connectionSocket.send(str(self.code).encode())
+        self.connectionSocket.send(str(self.message).encode())
+        self.connectionSocket.send(str(self.body).encode())
+
+    def aguarda_requisicao(self):
+        while True:
+            self.connectionSocket, self.addr = self.serverSocket.accept()
+
+            try:
+                self.recebe_requisicao()
+                self.realiza_requisicao()
+                self.enviar_resposta()
+
+
+            except BufferError:
+                self.code = 3
+                self.message = "Tamanho do arquivo para ser escrito maior que tamanho máximo permitido"
+
+            self.connectionSocket.close()
+
+    def recebe_requisicao(self):   
+        self.op = int(self.connectionSocket.recv(1).decode())
+        self.length = int(self.connectionSocket.recv(6).decode())
+        self.filename = self.connectionSocket.recv(64).decode()
+        self.path = self.connectionSocket.recv(128).decode()
+        self.body = self.connectionSocket.recv(self.length).decode()       
+
+
+def main():
+    server = PTATServer()
+
+    while True:
+        horario, op, path, code = server.aguarda_requisicao()
+
+        print(f"{horario}, {op}, {path}, {code}")
+        
+
+
+        
